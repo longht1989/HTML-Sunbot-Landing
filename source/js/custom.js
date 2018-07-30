a7480c7074b9a11414be4cc892b9b8937215e9d4
longht <longht@vng.com.vn>
2018-07-30 11:38:55 +0700

    udpate sunbot landing

---
+++ source/js/custom.js
@@ -0,0 +1,13 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+$(function() {
+    $('.slider ul').bxSlider({
+    	pager: 0,
+    	nextText:'',
+    	prevText:'',
+    	nextSelector:'.slider-next',
+    	prevSelector:'.slider-prev'
+    });
+});
