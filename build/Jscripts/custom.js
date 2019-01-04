a11c1107d083f9ef8f84513e600135fe6c1af326
longht <longht@vng.com.vn>
2019-01-04 14:41:56 +0700

    update sunbot

--- build/Jscripts/custom.js
+++ build/Jscripts/custom.js
@@ -4,10 +4,15 @@
 
 $(function() {
     $('.slider ul').bxSlider({
-    	pager: 0,
-    	nextText:'',
-    	prevText:'',
-    	nextSelector:'.slider-next',
-    	prevSelector:'.slider-prev'
+        pager: 0,
+        nextText: '',
+        prevText: '',
+        nextSelector: '.slider-next',
+        prevSelector: '.slider-prev',
+        minSlides: 2,
+        maxSlides: 2,
+        slideWidth: 490,
+        slideMargin: 20,
+        moveSlides: 1
     });
 });
